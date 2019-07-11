60d0f4221c6ef072567fae62db6aea7e31d9e2d0
vineet <vineet.agrawal100@gmail.com>
2019-07-11 17:36:55 +0530

    branch commit | constructor inj. scope prototype

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -16,19 +16,41 @@ public class Main
 {
     public static void main( String[] args )
     {
-        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie1 = xmlBeanFactory.getBean("moviedetails1",Movie.class);
+        //XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        //Movie movie1 = xmlBeanFactory.getBean("moviedetails1",Movie.class);
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2 = applicationContext.getBean("moviedetails2",Movie.class);
 
-        BeanDefinitionRegistry beanDefinitionRegistry = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        Movie movie3 = ((XmlBeanFactory)xmlBeanFactory).getBean("moviedetails2",Movie.class);
+        Movie movie2 = applicationContext.getBean("movie1",Movie.class);
+        Movie movie1 = applicationContext.getBean("movie2",Movie.class);
 
-        System.out.println(movie1.toString());
-        System.out.println(movie2.toString());  //print the same thing twice
-        System.out.println(movie3.toString());  //print the same thing twice
+        //for scope as prototype
+        Movie movie3 = applicationContext.getBean("movie3",Movie.class);
+        Movie movie4 = applicationContext.getBean("movie4",Movie.class);
+
+        //for bean name
+        Movie movie5 = applicationContext.getBean("MovieA",Movie.class);
+        Movie movie6 = applicationContext.getBean("MovieB",Movie.class);
+
+        //BeanDefinitionRegistry beanDefinitionRegistry = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        //XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(beanDefinitionRegistry);
+        //Movie movie3 = ((XmlBeanFactory)xmlBeanFactory).getBean("moviedetails2",Movie.class);
+
+        //System.out.println(movie1.toString());
+        //System.out.println(movie2.toString());  //print the same thing twice
+        //System.out.println(movie3.toString());  //print the same thing twice
+
+        System.out.println(movie2.toString());
+        System.out.println(movie1 == movie2);
+
+        //for scope prototype
+        System.out.println(movie3.toString());
+        System.out.println(movie3 == movie4);
+
+        //get bean by name, not id
+        System.out.println(movie5.toString());
+        System.out.println(movie5 == movie6);
+
 
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -3,6 +3,17 @@ package com.stackroute.domain;
 public class Movie {
 
     Actor actor;
+/*
+    String author;
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+*/
 
     public void setActor(Actor actor) {
         this.actor = actor;
